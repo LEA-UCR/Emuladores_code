383f2e0ba9297d97d873425fe98dfde091792431
Luis Barboza <lbarbo@gmail.com>
2019-12-17 11:11:18 -0600

    Creacion de rutinas para simulacion independiente de datos

--- 2.MCMC_MRA_general.R
+++ 2.MCMC_MRA_general.R
@@ -1,13 +1,14 @@
 library(tictoc)
-source("1.MRA_resolution.R")
+source("1.MRA_resolution_general.R")
 source('covariances.R')
-source('likelihoodK.R')
+source('likelihoodK_general.R')
 
 
 # values used to generate the data
-kappa <- 1.5
-sigma2 <- 1/4
-taue <- 1/4
+nu <- 1.5
+range <- 4
+sigma2 <- 1
+taue <- 20
 betas <- 1
 type='Exponential'
 
@@ -26,7 +27,7 @@ toc()
 
 #A k,l j_1,...,j_M-1
 #omega k j_1,...,j_M-1
-  
+
 #Ktilde j_1,...,j_M-1
 
 #dj_1,...,j_M-1
