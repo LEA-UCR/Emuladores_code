4573f70c87d207878d3d255a497a0e11f30a20a0
marcela <marcela.alfarocordoba@ucr.ac.cr>
2020-02-07 18:59:24 -0600

    3 parameters

--- simulation2/run.R
+++ simulation2/run.R
@@ -34,39 +34,45 @@ nn<-aa[[4]];hh<-aa[[5]]
 
 # initial values
 
-taub <- 1
-taue <- 10
+phi <- 0.9
+beta0 <- 0
+beta1 <- 2
 
-startvalue <- c(taub,taue)
+startvalue <- c(phi,beta0,beta1)
 N <- dim(hh)[1]
 npar <- length(startvalue)
 
 # fixed
-phi <- 0.9
+taub <- 1
+taue <- 10
 nu <- 1.5
-beta0 <- 0
-beta1 <- 2
+
 
 ##################
 # L functions    #
 ##################
 
 f <- function(param) {
-  taub <- param[1]
-  taue <- param[2]
+  phi <- param[1]
+  beta0 <- param[2]
+  beta1 <- param[3]
   sigma2 <- 1/taub
   if(analysis=="M1"){
-    loglike <- likelihoodGaussian(nu,phi,beta0,beta1,sigma2,taue,model,type)}
+    loglike <- likelihoodGaussian(nu,phi,beta0,
+                  beta1,sigma2,taue,model,type)}
   if(analysis=="M2"){
-    loglike <- likelihoodBanerjee(nu,phi,beta0,beta1,sigma2,taue,model,type)}
-  else{loglike <- likelihoodFSA_Block(nu,phi,beta0,beta1,sigma2,taue,model,type)}
+    loglike <- likelihoodBanerjee(nu,phi,beta0,
+                  beta1,sigma2,taue,model,type)}
+  else{loglike <- likelihoodFSA_Block(nu,phi,beta0,
+                  beta1,sigma2,taue,model,type)}
   #loglike <- likelihood(nu,phi,beta0,beta1,1/taub,taue,model,type)
   ## incluir previas para taue y taub (según Demirhan et al)
-  logpriortaue <- (dgamma(taub,shape=0.5, scale=2, log=T))
-  #logpriortaue <- log(dinvgamma(taue,shape=5, scale=5))
-  logpriortaub <- (dgamma(taub,shape=5, scale=2, log=T))
-  #logpriorphi <- dunif(phi,0.1,3,log=TRUE) 
-  logprior <- logpriortaue+logpriortaub#+logpriorphi
+  #logpriortaue <- (dgamma(taub,shape=0.5, scale=2, log=T))
+  #logpriortaub <- dgamma(taub,shape=5, scale=2, log=T)
+  logpriorphi <- dunif(phi,0.1,1.2,log=TRUE) 
+  logpriorbeta0 <- dnorm(0,0.1,log=TRUE)
+  logpriorbeta1 <- dnorm(1.5,1,log=TRUE)
+  logprior <- logpriorphi+logpriorbeta0+logpriorbeta1
   like <- -(loglike/2) +logprior
   return(like)
 }
@@ -75,25 +81,32 @@ f <- function(param) {
 # Main M-H  loop #
 ##################
 
-th <- c(0.01,0.1)
-alphax <- 0.234
+th <- c(0.1,0.1,0.1)
 
 proposalfunction <- function(param,i,th){
   if (is.null(dim(param)[1])){
-    sd <- c(0.05,0.05)
+    sd <- c(0.5,0.5,0.5)
     mu <- param
   }else{
     sd <- apply(param,2,sd)
+    if (sum(sd) < 0.0001){
+      sd <- c(0.5,0.5,0.5)
+    }else{
+      sd <- sd
+    }
     mu <- param[i,]
   }
-  alpha <- c(mu[1]^2/(th[1]*sd[1]),mu[2]^2/(th[2]*sd[2]))
-  beta  <- c(th[1]*sd[1]/mu[1],th[2]*sd[2]/mu[2])
-  Yn <- c(rgamma(1,shape=alpha[1],scale=beta[1]),
-          rgamma(1,shape=alpha[2],scale=beta[2]))
-  return(list(Yn,alpha,beta))
+  #alpha <- c(mu[1]^2/(th[1]*sd[1]))
+  #beta  <- c(th[1]*sd[1]/mu[1])
+  Yn <- c(#rgamma(1,shape=alpha[1],scale=beta[1]),
+          runif(1,0.1,2),
+          rnorm(1,mu[2],sd[2]),
+          rnorm(1,mu[3],sd[3]))
+  return(Yn)
+  #return(list(Yn,alpha,beta))
 }
 
-#metrop(f, startvalue, nbatch = 1e2)
+#metrop(f, startvalue, 10000)
 
 run_metropolis_MCMC <- function(startvalue, iterations){
   chain = array(dim = c(iterations+1,npar))
@@ -101,15 +114,15 @@ run_metropolis_MCMC <- function(startvalue, iterations){
   for (i in 1:iterations){
     # iterations <- 10000;i<-1
     ## Decision
-    proposal_all <- proposalfunction(chain[c(1:i),],i,th)
-    proposal <- proposal_all[[1]]
-    alpha <- proposal_all[[2]]
-    beta <- proposal_all[[3]]
+    proposal <- proposalfunction(chain[c(1:i),],i,th)
+    #proposal <- proposal_all[[1]]
+    #alpha <- proposal_all[[2]]
+    #beta <- proposal_all[[3]]
     probab <- min(0,
-    f(proposal) + dgamma(chain[i,1],alpha[1],beta[1], log=TRUE)
-        + dgamma(chain[i,2],alpha[2],beta[2], log=TRUE)
-  -f(chain[i,]) - dgamma(proposal[1],alpha[1],beta[1], log=TRUE)
-        - dgamma(proposal[2],alpha[2],beta[2], log=TRUE))
+    f(proposal) -
+    #+ dgamma(chain[i,1],alpha[1],beta[1], log=TRUE)-
+      f(chain[i,]) )
+    #- dgamma(proposal[1],alpha[1],beta[1], log=TRUE))
     alphan <- exp(probab)
     if (log(runif(1)) <= probab){
       chain[i+1,] = proposal
@@ -120,6 +133,8 @@ run_metropolis_MCMC <- function(startvalue, iterations){
   }
   return(chain)
 }
+
+set.seed(10)
 chain = run_metropolis_MCMC(startvalue, 4000)
 burnIn = 50
 acceptance = 1-mean(duplicated(chain[-(1:burnIn),]));acceptance
@@ -128,21 +143,21 @@ acceptance = 1-mean(duplicated(chain[-(1:burnIn),]));acceptance
 
 png(filename=paste0("sim_res/plot",analysis,model,type,i,".png"))
 par(mfrow = c(2,npar))
-# hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of phi", xlab="True value = red line")
-# abline(v = mean(chain[-(1:burnIn),2]))
-# abline(v = 0.9, col="red" )
-hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of taub", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),2]))
-abline(v = 1, col="red" )
-hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of taue", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),2]) )
-abline(v = 10, col="red" )
-# plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of phi", )
-# abline(h = 0.9, col="red" )
-plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of taub", )
-abline(h = 1, col="red" )
-plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of taue", )
-abline(h = 10, col="red" )
+hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of phi", xlab="True value = red line")
+abline(v = mean(chain[-(1:burnIn),1]), col="green")
+abline(v = 0.9, col="red" )
+hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of beta0", xlab="True value = red line")
+abline(v = mean(chain[-(1:burnIn),2]), col="green")
+abline(v = 0, col="red" )
+hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of beta1", xlab="True value = red line")
+abline(v = mean(chain[-(1:burnIn),3]), col="green")
+abline(v = 2, col="red" )
+plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of phi", )
+abline(h = 0.9, col="red" )
+plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of beta0", )
+abline(h = 0, col="red" )
+plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of beta1", )
+abline(h = 2, col="red" )
 dev.off()
 
 save(chain, file=paste0("sim_res/chain",analysis,model,type,i,".Rdata"))
