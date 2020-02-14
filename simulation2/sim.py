333fce1249e92f8f665d94264e9315ca2a1411e6
marcela <marcela.alfarocordoba@ucr.ac.cr>
2020-02-14 16:36:34 -0600

    chaging loop for i=1

--- simulation2/sim.py
+++ simulation2/sim.py
@@ -1,8 +1,8 @@
 import os
 
-for i in range(1):
-    for type in ['Exponential', "Matern"]:
-       for model in ['SVC']:
-          for analyse in ["M1", "M3"]:
-              os.system("Rscript run.R %d %s %s %s > output_%d_%s_%s_%s.txt"%
-                  (i+1,type,model,analyse,i+1,type,model,analyse))
+i = 1
+for type in ['Exponential', "Matern"]:
+    for model in ['SVC']:
+        for analyse in ["M1", "M3"]:
+            os.system("Rscript run.R %d %s %s %s > output_%d_%s_%s_%s.txt"%
+                (i,type,model,analyse,i,type,model,analyse))
