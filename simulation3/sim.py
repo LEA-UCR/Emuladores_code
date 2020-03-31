682a7e798fafb9dec33c2618d95229be32fd1743
marcela <marcela.alfarocordoba@ucr.ac.cr>
2020-03-30 19:56:19 -0600

    index as variable

--- simulation3/sim.py
+++ simulation3/sim.py
@@ -1,8 +1,9 @@
 import os
+import sys
 
-i = 1
+i = sys.argv[1]
 for type in ['Exponential', "Matern"]:
     for model in ['SVC', "SVI"]:
         for analyse in ["M3"]:
-            os.system("Rscript run.R %d %s %s %s > output_%d_%s_%s_%s.txt"%
+            os.system("Rscript run.R %s %s %s %s > output_%s_%s_%s_%s.txt"%
                 (i,type,model,analyse,i,type,model,analyse))
