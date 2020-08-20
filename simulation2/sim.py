b4ae23fa1d580402e8846854127c85eb04262c81
marcela <marcela.alfarocordoba@ucr.ac.cr>
2020-08-20 15:44:39 -0600

    reorganize

--- simulation2/sim.py
+++
@@ -1,8 +0,0 @@
-import os
-
-i = 1
-for type in ['Exponential', "Matern"]:
-    for model in ['SVC']:
-        for analyse in ["M1", "M3"]:
-            os.system("Rscript run.R %d %s %s %s > output_%d_%s_%s_%s.txt"%
-                (i,type,model,analyse,i,type,model,analyse))
